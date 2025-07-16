2ca18035e18d1e0998565d143537ac202369264a
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2025-07-16 11:38:54 +0900

    Android app working

--- App.tsx
+++ App.tsx
@@ -1,11 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, TextInput, View } from "react-native";
+import { useState } from "react";
+import { sum } from "sum-lib";
 
 export default function App() {
+  const [a, setA] = useState("");
+  const [b, setB] = useState("");
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <TextInput
+        style={styles.input}
+        value={a}
+        onChangeText={setA}
+        selectTextOnFocus
+      />
+      <Text style={styles.text}>+</Text>
+      <TextInput
+        style={styles.input}
+        value={b}
+        onChangeText={setB}
+        selectTextOnFocus
+      />
+      <Text style={styles.text}>
+        = {sum(parseFloat(a), parseFloat(b)) || "?"}
+      </Text>
     </View>
   );
 }
@@ -13,8 +33,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "row",
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 20,
+    textAlign: "center",
+  },
+  input: {
+    fontSize: 20,
+    textAlign: "center",
+    minWidth: 100,
+    borderBottomWidth: 1,
+    borderColor: "#aaa",
+    margin: 10,
   },
 });
