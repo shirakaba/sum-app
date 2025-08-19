1a3efabe5077d4552283742ec5c273ba190fe20d
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2025-08-20 08:33:10 +0900

    try using threadsafe Node-API with newest NativeScript Node-API

--- metro.config.js
+++ metro.config.js
@@ -1,15 +1,33 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('expo/metro-config');
-const path = require('node:path');
+const { getDefaultConfig } = require("expo/metro-config");
+const path = require("node:path");
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
+// Referenced from:
+// https://github.com/facebook/metro/issues/1225
+//
+// Could alternatively try metro-resolver-symlinks:
+// https://github.com/microsoft/rnx-kit/tree/main/packages/metro-resolver-symlinks#readme
+config.resolver = {
+  ...config.resolver,
+  extraNodeModules: {
+    ...config.resolver.extraNodeModules,
+    "react-native-node-api": path.resolve(
+      __dirname,
+      "../react-native-node-api-modules/packages/host"
+    ),
+  },
+};
+
 config.watchFolders = [
   __dirname,
-  path.resolve(__dirname, '../sum-lib'),
-  path.resolve(__dirname, '../runtime-node-api'),
-  path.resolve(__dirname, '../runtime-node-api/packages/ios'),
+  path.resolve(__dirname, "../sum-lib"),
+  path.resolve(__dirname, "../napi-ios"),
+  path.resolve(__dirname, "../napi-ios/packages/ios"),
+  path.resolve(__dirname, "../react-native-node-api-modules"),
+  path.resolve(__dirname, "../react-native-node-api-modules/packages/host"),
 ];
 
 module.exports = config;
