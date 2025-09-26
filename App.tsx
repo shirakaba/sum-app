a11e4266534e492d39bc54f6777788ff8ee71d24
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2025-09-26 22:52:57 +0900

    woah, this all worked!

--- App.tsx
+++ App.tsx
@@ -11,6 +11,8 @@ export default function App() {
         title="Try NativeScript"
         onPress={() => {
           const block = () => {
+            console.log("block now running...");
+
             const alertController =
               UIAlertController.alertControllerWithTitleMessagePreferredStyle(
                 "Hype alert",
@@ -31,14 +33,35 @@ export default function App() {
             rootViewController.presentViewControllerAnimatedCompletion(
               alertController,
               true,
-              () => {}
+              () => {
+                console.log("Completion.");
+                console.log(`NSThread.isMainThread: ${NSThread.isMainThread}`);
+                console.log(
+                  `NSThread.currentThread.name: ${NSThread.currentThread.name}`
+                );
+
+                setTimeout(() => {
+                  console.log("timeout!");
+                }, 1000);
+              }
             );
           };
+
+          console.log(`NSThread.isMainThread: ${NSThread.isMainThread}`);
+          console.log(
+            `NSThread.currentThread.name: ${NSThread.currentThread.name}`
+          );
 
           // The semantics of this is "run this block on the main thread".
           // However, the NativeScript implementation isn't matching
           // expectations here.
+          console.log("before");
+
+          // addOperationWithBlock() is a non-blocking method, so we see the
+          // "after" log before we get the UIKit crash.
           NSOperationQueue.mainQueue.addOperationWithBlock(block);
+
+          console.log("after");
 
           // dispatch_async(dispatch_get_main_queue(), block);
         }}
