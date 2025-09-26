03346da4bdc2d6ce90eb38f04b68fcc92e7248ac
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2025-09-26 20:59:56 +0900

    comment back in and run app again

--- App.tsx
+++ App.tsx
@@ -7,11 +7,42 @@ export default function App() {
     <View style={styles.container}>
       <StatusBar style="auto" />
 
-      <Button title="Try NativeScript" onPress={() => {
-        const hello = NSString.alloc().initWithString("Hello");
-        const helloWorld = hello.stringByAppendingString(', world!');
-        console.log(helloWorld);
-      }} />
+      <Button
+        title="Try NativeScript"
+        onPress={() => {
+          const block = () => {
+            const alertController =
+              UIAlertController.alertControllerWithTitleMessagePreferredStyle(
+                "Hype alert",
+                "We just accessed an arbitrary UIKit API on the UI thread from JS! 🥳",
+                UIAlertControllerStyle.Alert
+              );
+            alertController.addAction(
+              UIAlertAction.actionWithTitleStyleHandler(
+                "Sweet",
+                UIAlertActionStyle.Default,
+                () => {}
+              )
+            );
+            const rootViewController =
+              UIApplication.sharedApplication.keyWindow.rootViewController;
+
+            // We're confident we're on the JS thread, not the UI thread here.
+            rootViewController.presentViewControllerAnimatedCompletion(
+              alertController,
+              true,
+              () => {}
+            );
+          };
+
+          // The semantics of this is "run this block on the main thread".
+          // However, the NativeScript implementation isn't matching
+          // expectations here.
+          NSOperationQueue.mainQueue.addOperationWithBlock(block);
+
+          // dispatch_async(dispatch_get_main_queue(), block);
+        }}
+      />
     </View>
   );
 }
