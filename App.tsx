68ba21be6fa1c7741605b3a3742e7737af2e3264
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2025-09-26 23:52:48 +0900

    demonstrate nested thread-hopping

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View, Button } from "react-native";
-import { useEffect, useState } from "react";
 
 export default function App() {
   return (
@@ -29,7 +28,6 @@ export default function App() {
             const rootViewController =
               UIApplication.sharedApplication.keyWindow.rootViewController;
 
-            // We're confident we're on the JS thread, not the UI thread here.
             rootViewController.presentViewControllerAnimatedCompletion(
               alertController,
               true,
@@ -37,12 +35,53 @@ export default function App() {
                 console.log("Completion.");
                 console.log(`NSThread.isMainThread: ${NSThread.isMainThread}`);
                 console.log(
-                  `NSThread.currentThread.name: ${NSThread.currentThread.name}`
+                  `NSThread.currentThread.name: "${NSThread.currentThread.name}"`
                 );
 
                 setTimeout(() => {
-                  console.log("timeout!");
-                }, 1000);
+                  console.log(
+                    `timeout 1! NSThread.currentThread.name: "${NSThread.currentThread.name}"`
+                  );
+
+                  NSOperationQueue.mainQueue.addOperationWithBlock(() => {
+                    const alertController =
+                      UIAlertController.alertControllerWithTitleMessagePreferredStyle(
+                        "Hype alert 2",
+                        "We just accessed an arbitrary UIKit API on the UI thread from JS! 🥳",
+                        UIAlertControllerStyle.Alert
+                      );
+                    alertController.addAction(
+                      UIAlertAction.actionWithTitleStyleHandler(
+                        "Sweet",
+                        UIAlertActionStyle.Default,
+                        () => {}
+                      )
+                    );
+                    const rootViewController =
+                      UIApplication.sharedApplication.keyWindow
+                        .rootViewController;
+
+                    rootViewController.presentViewControllerAnimatedCompletion(
+                      alertController,
+                      true,
+                      () => {
+                        console.log("Completion.");
+                        console.log(
+                          `NSThread.isMainThread: ${NSThread.isMainThread}`
+                        );
+                        console.log(
+                          `NSThread.currentThread.name: ${NSThread.currentThread.name}`
+                        );
+
+                        setTimeout(() => {
+                          console.log(
+                            `timeout 2! NSThread.currentThread.name: "${NSThread.currentThread.name}"`
+                          );
+                        }, 1000);
+                      }
+                    );
+                  });
+                }, 5000);
               }
             );
           };
@@ -52,18 +91,13 @@ export default function App() {
             `NSThread.currentThread.name: ${NSThread.currentThread.name}`
           );
 
-          // The semantics of this is "run this block on the main thread".
-          // However, the NativeScript implementation isn't matching
-          // expectations here.
           console.log("before");
 
           // addOperationWithBlock() is a non-blocking method, so we see the
-          // "after" log before we get the UIKit crash.
+          // "after" log before the "block now running..." log.
           NSOperationQueue.mainQueue.addOperationWithBlock(block);
 
           console.log("after");
-
-          // dispatch_async(dispatch_get_main_queue(), block);
         }}
       />
     </View>
