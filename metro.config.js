0709a20a9813c41a21f2d91c54454b62c5e8d001
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2025-10-01 10:14:50 +0900

    try to align with Expo Orbit
    https://github.com/expo/orbit/blob/2c00526f8ebfd30e6f487727658c6b5ab837cbaf/apps/menu-bar/metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -23,11 +23,45 @@ config.resolver = {
 
 config.watchFolders = [
   __dirname,
+  path.resolve(__dirname, "./node_modules"),
   path.resolve(__dirname, "../sum-lib"),
   path.resolve(__dirname, "../napi-ios"),
   path.resolve(__dirname, "../napi-ios/packages/ios"),
   path.resolve(__dirname, "../react-native-node-api-modules"),
   path.resolve(__dirname, "../react-native-node-api-modules/packages/host"),
 ];
+
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  if (
+    platform === "macos" &&
+    (moduleName === "react-native" || moduleName.startsWith("react-native/"))
+  ) {
+    const newModuleName = moduleName.replace(
+      "react-native",
+      "react-native-macos"
+    );
+    return context.resolveRequest(context, newModuleName, platform);
+  }
+  return context.resolveRequest(context, moduleName, platform);
+};
+
+config.transformer.getTransformOptions = async () => ({
+  transform: {
+    experimentalImportSupport: true,
+    inlineRequires: true,
+  },
+});
+
+const originalGetModulesRunBeforeMainModule =
+  config.serializer.getModulesRunBeforeMainModule;
+config.serializer.getModulesRunBeforeMainModule = () => {
+  try {
+    return [
+      require.resolve("react-native/Libraries/Core/InitializeCore"),
+      require.resolve("react-native-macos/Libraries/Core/InitializeCore"),
+    ];
+  } catch {}
+  return originalGetModulesRunBeforeMainModule();
+};
 
 module.exports = config;
